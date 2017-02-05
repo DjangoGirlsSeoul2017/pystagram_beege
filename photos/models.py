9578820ad2052dfb7f04b76477f1941dda38c1f5
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-05 01:29:51 -0800

    Registered Photo model with Admin site. Made fix in Photo model to allow empty content.

--- photos/models.py
+++ photos/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Photo(models.Model):
     image = models.ImageField()
     filtered_image = models.ImageField()
-    content = models.TextField(max_length=500)
+    content = models.TextField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
