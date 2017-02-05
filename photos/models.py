5f91a9ecbe761b752e26905fe8a5708d0b2630d1
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-05 01:22:33 -0800

    Fixed problem in Photo model. Created first migration. Created requirements file.

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Photo(models.Model):
-    id = models.UUIDField()
     image = models.ImageField()
     filtered_image = models.ImageField()
     content = models.TextField(max_length=500)
