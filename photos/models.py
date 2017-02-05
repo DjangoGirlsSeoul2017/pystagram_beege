0818f5ac15d0decf889d5d9ceabb906d7c04b907
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-05 01:46:40 -0800

    Altered Photo model to upload to an uploads folder. Updated gitignore to include uploads folder.

--- photos/models.py
+++ photos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Photo(models.Model):
-    image = models.ImageField()
-    filtered_image = models.ImageField()
+    image = models.ImageField(upload_to='uploads/%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered')
     content = models.TextField(max_length=500, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
