8ccfa2e9286e9d9b61a00d668516e8b4d8dbfe21
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-05 01:48:34 -0800

    Updated Photo model to delete image files when it is deleted.

--- photos/models.py
+++ photos/models.py
@@ -6,3 +6,8 @@ class Photo(models.Model):
     filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered')
     content = models.TextField(max_length=500, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        self.filtered_image.delete()
+        super(Photo, self).delete(*args, **kwargs)
