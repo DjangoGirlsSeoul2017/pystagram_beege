508aef35efe4b2fe330ad1d65b7804f2ebb6c068
Bryan "Beege" Berry <bryanberry235711@gmail.com>
2017-02-19 01:13:14 -0800

    Login and logout now works, though no permissions scheme is yet enforced.

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/upload_files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
+LOGIN_REDIRECT_URL = '/photos/upload/'
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from photos.views import hello, detail, create
 
 urlpatterns = [
@@ -24,4 +25,20 @@ urlpatterns = [
     url(r'^hello/$', hello),
     url(r'^photos/(?P<pk>[0-9]+)/$', detail, name='detail'),
     url(r'^photos/upload/$', create, name='create'),
+url(
+        r'^accounts/login/',
+        auth_views.login,
+        name='login',
+        kwargs={
+            'template_name': 'login.html'
+        }
+    ),
+    url(
+        r'^accounts/logout/',
+        auth_views.logout,
+        name='logout',
+        kwargs={
+            'next_page': settings.LOGIN_URL,
+        }
+    ),
 ] + static('upload_files', document_root=settings.MEDIA_ROOT)
